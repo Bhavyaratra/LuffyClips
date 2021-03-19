ca12cba09f78c374b58baa62abf373b8899d90ab
Bhavya Ratra <bhavya.r1220@gmail.com>
2021-03-19 21:29:03 +0530

    created register model

---
+++ models/register.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const playerSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+},{timestamps: true});
+
+const Register = new mongoose.model('Registration', playerSchema);
+module.exports = Register;
